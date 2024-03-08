ca0c3fdb86a5b45dac9f25de71d28c6fcdcf41cf
V-VigneshR <vigneshvikash5gmail.com>
2024-03-08 19:18:57 +0530

    Flashblog updated with database

--- backend/flaskblog.py
+++ backend/flaskblog.py
@@ -1,9 +1,40 @@
+import os
+from datetime import datetime #for current datetime
 from flask import Flask,render_template,url_for,flash,redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm,LoginForm
 
-app = Flask(__name__) 
 
+app = Flask(__name__) 
 app.config['SECRET_KEY']='474534cac076e805a1ac91b2d683a8e3'
+#path for database
+instance_path = os.path.join(app.root_path, 'instance')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(instance_path, 'site.db')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+#instance for database
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True) #backref to access the user who created the post(post.author->displays author of the post)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+   
 
 posts= [
     {
